9a38c53e73bf2489b4ea908d7a9e2f8c98432db8
alalalalalalalalalalalalalalala <ana.presa.thome@escola.pr.gov.br>
2026-06-25 10:39:46 -0300

    .

---
+++ ativiade-arrays.js
@@ -0,0 +1,28 @@
+//const notas push pop me
+
+const notas = [10, 6, 8];
+
+notas.push(7);
+
+const media = (notas[0] + notas [1] + notas [2] + notas[3] ) / notas.length;
+
+console.log(media);
+
+//const nota1 = 10
+//const nota2 = 6.5
+//const nota3 = 8
+//const nota4 = 7.5
+
+const notas = [ 10, 6.5, 8, 7.5 ];
+
+const media = ( notas[0] + notas[1] + notas[2] + notas[3] )
+
+console.log (media);
+console.log (notas[3]);
+
+
+notas.pop();
+
+const media = (notas[0] + notas [1] + notas [2] + notas[3] ) / notas.length;
+
+console.log(media);
